9d94013fadd4d0e2aec3d82cfcaa51316f7d0898
Dominik <65778271+1ordNero@users.noreply.github.com>
2026-08-15 00:12:17 +0300

    Remove redundant character preload script

--- character-preload-v1.js
+++
@@ -1,5 +0,0 @@
-(()=>{
-const portraits=['human-warrior-male.webp','human-warrior-female.webp','human-mage-male.webp','human-mage-female.webp','human-warlock-male.webp','human-warlock-female.webp','human-druid-male.webp','human-druid-female.webp'].map(n=>`assets/characters/${n}`);
-const preload=()=>portraits.forEach(src=>{const img=new Image();img.decoding='async';img.src=src});
-if('requestIdleCallback'in window)requestIdleCallback(preload,{timeout:2500});else setTimeout(preload,800);
-})();
