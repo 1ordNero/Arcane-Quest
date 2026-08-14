d8f0637ac59bea7206ccc978d1703889d27bf648
Dominik <65778271+1ordNero@users.noreply.github.com>
2026-08-15 00:13:35 +0300

    Remove obsolete story summary loader

--- assets/js/story-summary-loader-v1.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  const script = document.createElement('script');
-  script.src = 'assets/js/hide-story-summary-v1.js';
-  script.defer = true;
-  document.head.appendChild(script);
-})();
