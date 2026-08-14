963d3239ce018e1f397f632be5cea718d4b7d783
Dominik <65778271+1ordNero@users.noreply.github.com>
2026-08-14 22:57:40 +0300

    Add personal story summary cleanup loader

---
+++ assets/js/story-summary-loader-v1.js
@@ -0,0 +1,6 @@
+(() => {
+  const script = document.createElement('script');
+  script.src = 'assets/js/hide-story-summary-v1.js';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
