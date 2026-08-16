bd732e6e68e9c9417c45b380db40dde43885b908
Dominik <65778271+1ordNero@users.noreply.github.com>
2026-08-16 16:43:24 +0300

    Bump visible version to v0.10.3

--- app-version-v1.js
+++ app-version-v1.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='v0.10.2';
+const VERSION='v0.10.3';
 const buildMeta=document.querySelector('meta[name="build"]');
 if(buildMeta)buildMeta.content=VERSION;
 window.ARCANE_APP_VERSION=VERSION;
