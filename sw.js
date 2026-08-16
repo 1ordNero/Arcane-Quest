e31c860b88b01954343ca52800c3396bf6ed4779
Dominik <65778271+1ordNero@users.noreply.github.com>
2026-08-16 09:00:28 +0300

    Refresh cache for catacomb room art

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='arcane-quest-shell-v3';
+const CACHE='arcane-quest-shell-v4';
 const SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('arcane-quest-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
